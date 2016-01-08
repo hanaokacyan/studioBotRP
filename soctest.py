3f6f3a3c978584e14e0397f33ef39e88e5b894a0
hanaokacyan <cyberwoks@yahoo.co.jp>
2016-01-08 17:41:11 +0900

    test

--- soctest.py
+++ soctest.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python  
 import socket
-import serial
 
 def main():
   host = '192.168.1.53'
   port = 4000
   backlog = 10
   bufsize = 4096
-
-#  ser = serial.Serial('/dev/ttyUSB0' , 115200)
 
   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
   sock.bind((host, port))
