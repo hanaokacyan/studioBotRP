1158f340fd3107a6b82abf1ddaa6cea78fdaf2ae
hanaokacyan <cyberwoks@yahoo.co.jp>
2016-01-16 10:30:17 +0900

    Update autopilot.py

--- autopilot.py
+++ autopilot.py
@@ -17,7 +17,7 @@ for i in range(1): # loop count in range(n)
     time.sleep(0.5) # dulation 0.5sec
     # Step 3
     print("Backup")
-    ser.write("b 1000\r")
+    ser.write("b 0 1\r")
     time.sleep(1.0) # dulation 1.0sec
     # Step 4
     print("brake")
