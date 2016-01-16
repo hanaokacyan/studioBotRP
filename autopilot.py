1c8d7827ada00605407f56f2b8901909cd92a284
hanaokacyan <cyberwoks@yahoo.co.jp>
2016-01-16 10:27:33 +0900

    Update autopilot.py

--- autopilot.py
+++ autopilot.py
@@ -9,9 +9,19 @@ for i in range(1): # loop count in range(n)
     # Start sequence
     # Step 1
     print("forward")
-    ser.write("f 1000\r")
+    ser.write("f 0 1\r")
+    time.sleep(1.0) # dulation 1.0sec
     # Step 2
+    print("brake")
+    ser.write("s 0 1\r")
+    time.sleep(0.5) # dulation 0.5sec
+    # Step 3
     print("Backup")
     ser.write("b 1000\r")
+    time.sleep(1.0) # dulation 1.0sec
+    # Step 4
+    print("brake")
+    ser.write("s 0 1\r")
+    time.sleep(0.5) # dulation 0.5sec
 
 # End of program
