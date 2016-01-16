b7b31d810f01dc019ea01e2fe78cf4df3a34285d
hanaokacyan <cyberwoks@yahoo.co.jp>
2016-01-16 09:45:59 +0900

    Update autopilot.py

--- autopilot.py
+++ autopilot.py
@@ -5,12 +5,13 @@ import time
 # Open serial port
 ser = serial.Serial('/dev/ttyUSB0' , 115200)
 
-# Start sequence
-# Step 1
-print("forward")
-ser.write("f 1000\r")
-# Step 2
-print("Backup")
-ser.write("b 1000\r")
+for i in range(1) # loop count in range(n)
+    # Start sequence
+    # Step 1
+    print("forward")
+    ser.write("f 1000\r")
+    # Step 2
+    print("Backup")
+    ser.write("b 1000\r")
 
 # End of program
