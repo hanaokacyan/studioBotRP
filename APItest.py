7a12b21ceaf2b7ad0a98ed6a2eaa8c4e2d506404
hanaokacyan <cyberwoks@yahoo.co.jp>
2016-01-21 18:14:28 +0900

    Update APItest.py

--- APItest.py
+++ APItest.py
@@ -15,29 +15,30 @@ def brake(dur):
 def sensorALL():
   ser.flushInput() # clear serial receve buffer 
   ser.write("1\r")
-  l = int(ser.readline()) # wait for robot response
+  l = int(ser.readline()) # read Left sensor
   ser.write("2\r")
-  f = int(ser.readline()) # wait for robot response
+  f = int(ser.readline()) # read Front sensor
   ser.write("3\r")
-  r = int(ser.readline()) # wait for robot response
+  r = int(ser.readline()) # read Return sensor
   return(l,f,r)
 
 def main():
   global ser
   ser = serial.Serial('/dev/ttyUSB0' , 115200)
   
-  motor(1,20,1,20)
-  time.sleep(1)
-  brake(500)
-  motor(2,20,2,20)
-  time.sleep(1)
-  brake(500)
-  print("end fo program")
+  #motor(1,20,1,20)
+  #time.sleep(1)
+  #brake(500)
+  #motor(2,20,2,20)
+  #time.sleep(1)
+  #brake(500)
+  #print("end fo program")
   
-  #while True:
-    #l,f,r = sensorALL()
+  while True:
+    l,f,r = sensorALL()
     #print("SENSOR  %d : %d : %d" % (l,f,r))
-    #time.sleep(0.3)
+    print("SENSOR  " + chr(l) + " : " + chr(f) + " : " + chr(r))
+    time.sleep(0.3)
   
 
 if __name__ == '__main__':
