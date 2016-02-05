52f5502084ac96abb2084f521ddc01137f05cd26
hanaokacyan <cyberwoks@yahoo.co.jp>
2016-02-05 15:40:36 +0900

    Update autopilot.py

--- autopilot.py
+++ autopilot.py
@@ -9,19 +9,19 @@ for i in range(2): # loop count in range(n)
     # Start sequence
     # Step 1
     print("forward")
-    ser.write("f 0 1\r") # !!!Replace by your command
+    ser.write("m 1 32 1 32\r") # !!!Replace by your command
     time.sleep(1.0) # dulation 1.0sec
     # Step 2
     print("brake")
-    ser.write("s 0 1\r") # !!!Replace by your command
+    ser.write("m 3 32 3 32\r") # !!!Replace by your command
     time.sleep(0.5) # dulation 0.5sec
     # Step 3
     print("Backup")
-    ser.write("b 0 1\r") # !!!Replace by your command
+    ser.write("m 2 32 2 32\r") # !!!Replace by your command
     time.sleep(1.0) # dulation 1.0sec
     # Step 4
     print("brake")
-    ser.write("s 0 1\r") # !!!Replace by your command
+    ser.write("m 3 32 3 32\r") # !!!Replace by your command
     time.sleep(0.5) # dulation 0.5sec
 
 # End of program
